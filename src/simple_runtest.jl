340133bc726a872d7b4e160885fb03cca41a6b59
Anderson Schwertner <andersonschwertner@hotmail.com>
2021-10-20 11:08:11 -0300

    Changes the problem example

--- src/simple_runtest.jl
+++ src/simple_runtest.jl
@@ -2,21 +2,22 @@ using LOWDER
 
 function f(x)
 
-    return - 0.5 * x[1] + 10 * x[2]
+    return x[1] ^ 2.0 + x[2] ^ 2.0
 
 end
 
 function g(x)
 
-    return x[1] ^ 2.0 + x[2] ^ 2.0
+    return 0.5 * x[1] + 0.1 * x[2] + 1.0
 
 end
 
 fmin_list = [f, g]
-x = [2.0, 4.0]
-a = [0.0, 0.0]
-b = [5.0, 5.0]
-δ = 1.0
-Δ = 1.5
+x = [1.5, 1.0]
+a = [-1.0, -2.0]
+b = [1.5, 2.0]
+δ = 0.5
+Δ = 0.5
 
-sol = LOWDER.lowder(fmin_list, x, a, b, δ, Δ; m = 3, maxit = 100, maxfun = 75, verbose = 3, filename="./data_files/simple_runtest.dat")
+#sol = LOWDER.lowder(fmin_list, x, a, b, δ, Δ; m = 3, verbose = 3, filename="./data_files/simple_runtest_02.dat")
+sol = LOWDER.lowder(fmin_list, x, a, b, δ, Δ; m = 3)
