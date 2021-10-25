c56acf84ac9efccb2fc12a779a8a4f23717b541c
Francisco N. C. Sobral <fsobral@uem.br>
2021-10-25 15:49:32 -0300

    Add preliminary tests.

---
+++ src/tunning/autotunning_test.jl
@@ -0,0 +1,36 @@
+using Test
+
+@testset "getsymbols!" begin
+
+    ex = :(a)
+
+    s = Set()
+
+    @test(getsymbols!(ex, s) == ex)
+    @test(isempty(s))
+
+    ex = :(tunninginput(a))
+
+    s = Set()
+
+    @test(getsymbols!(ex, s) == :(a))
+    @test(ex == :(tunninginput(a)))
+    @test(s == Set([:a]))
+    
+    ex = :(f(tunninginput(a), b))
+
+    s = Set()
+
+    @test(getsymbols!(ex, s) == :(f(a, b)))
+    @test(ex == :(f(tunninginput(a), b)))
+    @test(s == Set([:a]))
+    
+    ex = :(f(cos(sin(tunninginput(a))), [tunninginput(b), tunninginput(c), 5]))
+
+    s = Set()
+    
+    @test(getsymbols!(ex, s) == :(f(cos(sin(a)), [b, c, 5])))
+    @test(ex == :(f(cos(sin(tunninginput(a))), [tunninginput(b), tunninginput(c), 5])))
+    @test(s == Set([:a, :b, :c]))
+    
+end
