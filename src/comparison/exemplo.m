819f0266c876267b67565eeaff363daeea6096fc
Anderson Schwertner <andersonschwertner@hotmail.com>
2022-09-08 20:20:14 -0300

    Moves files to other directory

--- src/comparison/exemplo.m
+++
@@ -1,5 +0,0 @@
-% Exemplo simples para instalar o MATDaemon, e verificar a instalação.
-% O resultado da chamada da função deve ser 55.
-
-jlcall('x -> sum(abs2, x)', {1:5}, 'server', false, ...
-    'runtime', '/usr/bin/julia')
--- src/comparison/teste.m
+++
@@ -1,6 +0,0 @@
-x = linspace(1, 1, 9)';
-
-[a, b] = restricoes(x);
-
-[f, g] = objetivo(x);
-
