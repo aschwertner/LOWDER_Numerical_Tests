d6158e7ce65f06585446c58556df4e3b33a0aebb
Anderson Schwertner <andersonschwertner@hotmail.com>
2022-10-05 17:25:50 -0300

    Adds log information and new stopping criterias

--- src/comparison/runGRANSOhs.m
+++ src/comparison/runGRANSOhs.m
@@ -1,4 +1,4 @@
-function sol = runGRANSOhs()
+function [sol, log] = runGRANSOhs()
 
     % Adds the path to the GRANSO solver.
     addpath("GRANSO/");
@@ -9,13 +9,20 @@ function sol = runGRANSOhs()
     file_directory_1 = strcat(current_directory, '/problem_global.jl');
     file_directory_2 = strcat(current_directory, '/comparison_hs.jl');
 
-    % Creates the file that will receive the execution data.
-    file_directory_3 = strcat(fileparts(fileparts(current_directory)), ...
-        '/data_files/hs_GRANSO_36.dat');
+    % Creates the file that will receive the execution data of GRANSO in
+    % HS testset.
+    directory = fileparts(fileparts(current_directory));
+    file_directory_3 = strcat(directory, ...
+        '/data_files/hs_GRANSO.dat');
     fileID = fopen(file_directory_3, 'w');
 
     % Selects problem 'np'.
-    for np = 1:87
+    for np = 1:1
+        
+        % Creates a file for each problem with log data.
+        file_directory_4 = strcat(directory, ...
+            '/data_files/HS/GRANSO/', int2str(np), '.dat');
+        fileID_2 = fopen(file_directory_4, 'w');
 
         try
 
@@ -37,15 +44,23 @@ function sol = runGRANSOhs()
             % Sets the equality constraint set to empty.
             eq_constraints = [];
 
-            % Sets tolerance for stationarity, maximum number of 
-            % iterations, and maximum clock time.
-            opts.opt_tol = 1e-4;
-            opts.maxit = 100 * nvar;
-            opts.maxclocktime = 1800;
+            % Sets tolerance for stationarity and for step length, maximum 
+            % number of iterations, and print level.
+            opts.opt_tol = 0;
+            opts.step_tol = 1e-20;
+            opts.maxit = 1100;
+            opts.print_level = 0;
+
+            % Sets halt and log functions.
+            [halt_log_fn, get_log_fn] = makeHaltLogFunctions(opts.maxit);
+            opts.halt_log_fn = halt_log_fn;
 
             % Calls the solver.
             sol = granso(nvar, @objective_func, @ineq_constraints, ...
                 eq_constraints, opts);
+
+            % Gets the log.
+            log = get_log_fn();
 
             % Computes the number of fi evaluations.
             fi_evals = sol.fn_evals * nfi;
@@ -56,6 +71,10 @@ function sol = runGRANSOhs()
                 sol.termination_code];
             fprintf(fileID,'%d %d %d %d %.4e %.4e %.4e %d\n', text);
 
+            % Saves info about log.
+            log_info = [log.f; log.tv; log.fn_evals];
+            fprintf(fileID_2, '%.7e %.7e %d\n', log_info);
+
             % Display info.
             text_display = strcat("Running problem ", string(np), ...
                 " ... success!");
@@ -65,13 +84,16 @@ function sol = runGRANSOhs()
 
             % Saves info about solution.
             fprintf(fileID, '%s\n', 'NaN NaN NaN NaN NaN NaN NaN NaN');
-            
+           
             % Display info.
             text_display = strcat("Running problem ", string(np), ...
                 " ... fail!");
             disp(text_display);
 
         end
+
+        % Close file.
+        fclose(fileID_2);
 
     end
 
@@ -103,5 +125,51 @@ function [c, cgrad] = ineq_constraints(x)
 
     % Computes the inequality constraints and its gradient.
     [c, cgrad] = jlcall('c_obj', {x}, 'setup', file_directory);
+
+end
+
+function [halt_log_fn, get_log_fn] = makeHaltLogFunctions(maxit)
+
+    % Function handles.
+    halt_log_fn     = @haltLog;
+    get_log_fn      = @getLog;
+
+    % Stores GRANSO history of function values.
+    index = 0;
+    sum_fn_evals = 0;
+    f = zeros(1,maxit+1);
+    tv = zeros(1,maxit+1);
+    fn_evals = zeros(1,maxit+1);
+
+    function halt = haltLog(    iter, x, penaltyfn_parts, d,        ...
+                                get_BFGS_state_fn, H_regularized,   ...
+                                ls_evals, alpha,                    ...
+                                n_gradients, stat_vec, stat_val,    ...
+                                fallback_level                      )
+          
+        % Increment the index/count. 
+        index = index + 1;
+        sum_fn_evals = sum_fn_evals + ls_evals;
+        
+        % Stores history data.
+        f(index) = penaltyfn_parts.f;
+        tv(index) = penaltyfn_parts.tv;
+        fn_evals(index) = sum_fn_evals;
+        
+        % keep this false unless you want to implement a custom termination
+        % condition
+        if sum_fn_evals >= maxit
+            halt = true;
+        else
+            halt = false;
+        end
+
+    end
+
+    function log = getLog()
+        log.f   = f(1:index);
+        log.tv  = tv(1:index);
+        log.fn_evals = fn_evals(1:index);
+    end
 
 end
