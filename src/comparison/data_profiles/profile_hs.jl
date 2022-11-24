2b94eeaad302b1cc77e7914af7224c19e2c3345e
Anderson Schwertner <andersonschwertner@hotmail.com>
2022-11-24 20:23:18 -0300

    Ignores problems that the solver fail to solve

--- src/comparison/data_profiles/profile_hs.jl
+++ src/comparison/data_profiles/profile_hs.jl
@@ -32,10 +32,17 @@ function generate_data_profile_hs()
 
     # Generates the data profiles for the specified gates.
     gate = [1.0e-1, 1.0e-3, 1.0e-5, 1.0e-7]
+    #for i in eachindex(gate)
+    #    data_profile(H, N, solvers_names; τ=gate[i])
+    #    savefig("./images/data_profile_hs_$(i).png")
+    #end
+
+    images_vec = Vector(undef, length(gate))
     for i in eachindex(gate)
-        data_profile(H, N, solvers_names; τ=gate[i])
-        savefig("./images/data_profile_hs_$(i).png")
+        images_vec[i] = data_profile(H, N, solvers_names; τ=gate[i])
     end
+    plot(images_vec[1], images_vec[2], images_vec[3], images_vec[4], layout = length(gate), size=(1200, 800))
+    savefig("./images/original_version/data_profile_hs.png")
 
 end
 
@@ -51,6 +58,8 @@ function create_matrix_hs(n_prob, n_feval, solvers_names, P)
 
     for solver in eachindex(solvers_names)
 
+        data_solver = readdlm(directory * "/data_files/HS/$(solvers_names[solver]).dat")[:, 2]
+
         for problem=1:n_prob
 
             if solvers_names[solver] == "LOWDER"
@@ -61,13 +70,17 @@ function create_matrix_hs(n_prob, n_feval, solvers_names, P)
             
             try
 
-                data_filename = directory * "/data_files/HS/$(solvers_names[solver])/$(problem).dat"
-                data_raw = readdlm(data_filename)
+                if data_solver[problem] == "success"
 
-                n_reg = min(length(data_raw), n_feval)
+                    data_filename = directory * "/data_files/HS/$(solvers_names[solver])/$(problem).dat"
+                    data_raw = readdlm(data_filename)
 
-                for i=1:n_reg
-                    data[Int(factor * i), problem, solver] = data_raw[i]
+                    n_reg = min(length(data_raw), n_feval)
+
+                    for i=1:n_reg
+                        data[Int(factor * i), problem, solver] = data_raw[i]
+                    end
+
                 end
                
             catch
